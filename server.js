dd9d6e33b51e944b2bee57252e6bfbb0acb08738
alevfalse <38272871+alevfalse@users.noreply.github.com>
2018-05-25 13:05:44 +0800

    unstaged and ignore package files

--- server.js
+++ server.js
@@ -9,18 +9,23 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+app.use(express.static('public'))
+
 var db = null
 var dbuser = config["database"]["username"]
 var dbpw = config["database"]["password"]
 console.log('DBUser: ' + dbuser + " | DBPass: " + dbpw)
 var dburl = 'mongodb://' + dbuser + ':' + dbpw + '@ds014648.mlab.com:14648/tumblr'
-MongoClient.connect(dburl, (err, client) => {
+
+/*MongoClient.connect(dburl, (err, client) => {
     if (err) return console.log(err)
 
     db = client.db('star-wars-quotes')
-    app.listen(3000, function () {
-        console.log('Listening to 3000!')
-    })
+    
+})*/
+
+app.listen(3000, function () {
+    console.log('Listening to 3000!')
 })
 
 app.get('/', (req, res) => {
